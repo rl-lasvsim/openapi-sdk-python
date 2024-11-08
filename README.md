e90d5b5be24ee061fb8500cfe67b2ffeca1b5546
luozhihong <luozhihong@risenlighten.com>
2024-11-08 11:10:08 +0800

    add: push to pip comment.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 test
+## 发布pip流程
+```
+1. 配置好setup.py
+2. 安装依赖：pip install twine setuptools wheel
+3. 构建分发包：python setup.py sdist bdist_wheel
+4. 上传包到 PyPI：twine upload dist/*
+```
--- setup.py
+++ setup.py
@@ -4,17 +4,27 @@ from setuptools import find_packages
 # with open("README.md", mode="r", encoding=UTF_8) as f:
 #     readme = f.read()
 
+# 从 requirements.txt 文件中读取依赖项
+
+
+def parse_requirements(filename):
+    with open(filename, "r") as file:
+        return [line.strip() for line in file if line.strip() and not line.startswith("#")]
+
+
 setup(
     name="qianxing-openapi",
-    # version=VERSION,
+    version="0.1.0",
     description="Qianxing OpenAPI SDK for Python",
     # long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
     author="Zhihong Luo",
     author_email="luozhihong@risenlighten.com",
     url="https://github.com/risenlighten-qianxing/openapi-sdk-python",
     packages=find_packages(),
-    install_requires=["requests", "protobuf>3,<4", "httpx"],
+    install_requires=[
+        "requests",
+    ],
     python_requires=">=3.0",
     keywords=["Qianxing", "Lasvsim", "自动驾驶", "OpenAPI"],
     include_package_data=True,
